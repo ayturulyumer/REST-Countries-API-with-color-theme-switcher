880a539645eb76e543e116f379f18e1268b2288c
ajummer <makeudreamempty@gmail.com>
2024-02-09 13:48:09 +0200

    Add SearchBar and Filter in Main

--- rest-countries/src/components/Main/Main.jsx
+++ rest-countries/src/components/Main/Main.jsx
@@ -1,8 +1,12 @@
-import SearchBar from "../SearchBar/SearchBar.jsx"
-import styles from "./main.module.css"
+import SearchBar from "../SearchBar/SearchBar.jsx";
+import Filter from "../Filter/Filter.jsx";
+import styles from "./main.module.css";
 
 export default function Main() {
   return (
-    <SearchBar/>
-  )
+    <main className={styles.container}>
+      <SearchBar />
+      <Filter/>
+    </main>
+  );
 }
