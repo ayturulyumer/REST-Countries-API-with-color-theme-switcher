b8859f98e042612b7ab3a38886dd45989d4697bc
ajummer <makeudreamempty@gmail.com>
2024-02-10 13:36:24 +0200

    Seperate Main into upper and main

--- rest-countries/src/components/Main/Main.jsx
+++ rest-countries/src/components/Main/Main.jsx
@@ -1,12 +1,18 @@
 import SearchBar from "../SearchBar/SearchBar.jsx";
 import Filter from "../Filter/Filter.jsx";
 import styles from "./main.module.css";
+import Card from "../Card/Card.jsx";
 
 export default function Main() {
   return (
-    <main className={styles.container}>
-      <SearchBar />
-      <Filter/>
-    </main>
+    <>
+      <div className={styles.upper}>
+        <SearchBar />
+        <Filter />
+      </div>
+      <div className={styles.main}>
+       <Card/>
+      </div>
+    </>
   );
 }
