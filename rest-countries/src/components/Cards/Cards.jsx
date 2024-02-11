2cda00e0f9d48a9741fb182a0dcc1a3a40d53b30
ajummer <makeudreamempty@gmail.com>
2024-02-11 14:14:57 +0200

    Get Countries from api and set in state

--- rest-countries/src/components/Cards/Cards.jsx
+++ rest-countries/src/components/Cards/Cards.jsx
@@ -1,10 +1,22 @@
-import styles from "./cards.module.css"
-
+import styles from "./cards.module.css";
+import Card from "../Card/Card.jsx";
+import { useState, useEffect } from "react";
+import * as countriesApi from "../../api/countriesApi.js";
 
 export default function Cards() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    countriesApi
+      .getCountries()
+      .then((result) => setCountries(result))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className={styles.container}>
-
+      {countries.map((country, index) => (
+        <Card key={index} country={country} />
+      ))}
     </div>
-  )
+  );
 }
