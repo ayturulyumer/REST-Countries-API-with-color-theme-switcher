642174b82045d6ba19567e0aae7b036329aecbc9
ajummer <makeudreamempty@gmail.com>
2024-02-11 13:36:13 +0200

    Add requester for api calls

---
+++ rest-countries/src/api/countriesApi.js
@@ -0,0 +1,8 @@
+import * as request from "../lib/request.js";
+const initialCountries =
+  "https://restcountries.com/v3.1/all?fields=flags,name,population,capital";
+
+export const getCountries = async () => {
+  const result = request.get(initialCountries);
+  return result;
+};
