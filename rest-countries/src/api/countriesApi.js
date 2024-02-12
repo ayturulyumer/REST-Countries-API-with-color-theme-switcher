30a866c9e0f8b474e26581ebc64598cf2b61a038
ajummer <makeudreamempty@gmail.com>
2024-02-12 10:43:22 +0200

    Add region to query

--- rest-countries/src/api/countriesApi.js
+++ rest-countries/src/api/countriesApi.js
@@ -1,6 +1,6 @@
 import * as request from "../lib/request.js";
 const initialCountries =
-  "https://restcountries.com/v3.1/all?fields=flags,name,population,capital";
+  "https://restcountries.com/v3.1/all?fields=flags,name,population,capital,region";
 
 export const getCountries = async () => {
   const result = request.get(initialCountries);
