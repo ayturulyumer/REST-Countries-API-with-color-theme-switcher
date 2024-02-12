eeb1bd25b1247925d7de2467f0068bca99331387
ajummer <makeudreamempty@gmail.com>
2024-02-12 20:32:36 +0200

    Add  get single country function

--- rest-countries/src/api/countriesApi.js
+++ rest-countries/src/api/countriesApi.js
@@ -1,8 +1,16 @@
 import * as request from "../lib/request.js";
-const initialCountries =
-  "https://restcountries.com/v3.1/all?fields=flags,name,population,capital,region";
+const baseUrl = "https://restcountries.com/v3.1";
 
 export const getCountries = async () => {
-  const result = request.get(initialCountries);
+  const result = request.get(
+    `${baseUrl}/all?fields=flags,name,population,capital,region`
+  );
+  return result;
+};
+
+export const getCountry = async (countryName) => {
+  const result = request.get(
+    `${baseUrl}/name/${countryName}?fields=name,flags,population,region,subregion,capital,tld,currencies,languages,borders`
+  );
   return result;
 };
