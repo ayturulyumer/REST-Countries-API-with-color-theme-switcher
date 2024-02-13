9057c206d37ceb8bb38bfe7141d58676ed3e47c9
ajummer <makeudreamempty@gmail.com>
2024-02-13 11:30:35 +0200

    Add more abstract approach
    Now the function also takes number of properties argument. If secondary argument is not passed it returns all the properties by default

--- rest-countries/src/utils/getObjFirstProperty.js
+++ rest-countries/src/utils/getObjFirstProperty.js
@@ -1,12 +1,20 @@
- export function getFirstProperty(obj) {
+ export function getFirstProperty(obj , numProperties) {
     // Check if the object is empty
     if (Object.keys(obj).length === 0) {
         return undefined; // Return undefined if the object is empty
     }
     
-    // Get the first property of the object
-    const firstKey = Object.keys(obj)[0];
-    
-    // Return the value of the first property
-    return obj[firstKey];
+    // If numProperties is not provided, default it to the number of properties in the object
+    if (numProperties === undefined) {
+        numProperties = Object.keys(obj).length;
+    }
+
+    // Get the properties of the object
+    const keys = Object.keys(obj);
+
+    // Calculate the actual number of properties to retrieve
+    const actualNumProperties = Math.min(numProperties, keys.length);
+
+    // Return the values of the specified number of properties
+    return keys.slice(0, actualNumProperties).map(key => obj[key]);
 }
