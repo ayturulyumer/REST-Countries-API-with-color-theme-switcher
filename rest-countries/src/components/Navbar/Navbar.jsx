a2f4da76e46caa7a70d840fb03f8bae54e1344fe
ajummer <makeudreamempty@gmail.com>
2024-02-13 12:34:26 +0200

    Add Link to header

--- rest-countries/src/components/Navbar/Navbar.jsx
+++ rest-countries/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
-import styles from "./navbar.module.css"
+import styles from "./navbar.module.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <header className={styles.header}>
-        <div className={styles.wrapper}>
-            <h1>Where in the world?</h1>
-            <span><i className="fa-regular fa-moon"></i>Dark Mode</span>
-        </div>
+      <div className={styles.wrapper}>
+        <Link to={"/"} style={{ textDecoration: "none", color: "inherit" }}>
+          <h1>Where in the world?</h1>
+        </Link>
+        <span>
+          <i className="fa-regular fa-moon"></i>Dark Mode
+        </span>
+      </div>
     </header>
-  )
+  );
 }
