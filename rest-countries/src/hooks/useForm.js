1404462ead6c1f053b2d8685e78fce3c42f6d1d9
ajummer <makeudreamempty@gmail.com>
2024-02-20 12:25:02 +0200

    Create useForm Hook

---
+++ rest-countries/src/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const useForm = (initialValues, onSubmitHandler) => {
+  const [values, setValues] = useState(initialValues);
+
+  const changeHandler = (e) => {
+    setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+    
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onSubmitHandler(values);
+
+    {/** Empty fields after submit */}
+    setValues(initialValues)
+  };
+
+  return {
+    values,
+    changeHandler,
+    onSubmit,
+  };
+};
