996f0d1b0983462a9889de642c7e7b08241dc7a7
ajummer <makeudreamempty@gmail.com>
2024-02-20 12:25:50 +0200

    Use useForm Hook to get values of  the form

--- rest-countries/src/components/SearchBar/SearchBar.jsx
+++ rest-countries/src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,25 @@
 import styles from "./searchbar.module.css";
+import { useForm } from "../../hooks/useForm.js";
 
 export default function SearchBar({ onSearchChange }) {
-  const changeHandler = (event) => {
-    onSearchChange(event.target.value);
-  };
+ 
+
+  const { values, changeHandler, onSubmit } = useForm(
+    {
+      searchInput: "",
+    },
+    onSearchChange
+  );
 
   return (
     <div className={styles.wrapper}>
-      <form className={styles.search} method="POST">
-        <i className="fa fa-search" />
+      <form className={styles.search} method="POST" onSubmit={onSubmit}>
+        <i className="fa fa-search"  onClick={onSubmit}/>
         <input
           type="text"
           name="searchInput"
           placeholder="Search for a country..."
+          value={values.searchInput}
           onChange={changeHandler}
         />
       </form>
