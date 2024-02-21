76698829fdb5d2a29d05fd35ec41f38941927033
ajummer <makeudreamempty@gmail.com>
2024-02-21 13:44:48 +0200

    Wrap app.jsx with themecontext

--- rest-countries/src/App.jsx
+++ rest-countries/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Main from "./components/Main/Main.jsx";
 import Details from "./components/Details/Details.jsx";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
 import "./app.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -9,11 +10,15 @@ function App() {
   return (
     <>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/details/:countryName" element={<Details />} />
-        </Routes>
+        <ThemeProvider>
+          <div className="App" id="light">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/details/:countryName" element={<Details />} />
+            </Routes>
+          </div>
+        </ThemeProvider>
       </Router>
     </>
   );
