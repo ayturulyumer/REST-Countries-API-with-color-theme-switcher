2cc3772fd9f9ae8648379aa9dcfa7daf5834a65f
ajummer <makeudreamempty@gmail.com>
2024-02-23 13:29:32 +0200

    Add Theme preference to local storage

--- rest-countries/src/contexts/ThemeContext.jsx
+++ rest-countries/src/contexts/ThemeContext.jsx
@@ -1,13 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    // Get the theme from local storage, if it exists
+    const storedTheme = localStorage.getItem("theme");
+    // Return the stored theme, or "light" if no theme is stored
+    return storedTheme ? storedTheme : "light";
+  });
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
-    // Optionally, save theme preference to local storage
+    setTheme((curr) => {
+      const newTheme = curr === "light" ? "dark" : "light";
+      // Save the new theme preference to local storage
+      localStorage.setItem("theme", newTheme);
+      return newTheme;
+    });
   };
 
   const value = {
