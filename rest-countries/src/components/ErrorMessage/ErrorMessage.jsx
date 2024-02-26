d3bd6f3f4e4e6f0b522213e960bfa0164597c823
ajummer <makeudreamempty@gmail.com>
2024-02-26 21:49:07 +0200

    add data testid

--- rest-countries/src/components/ErrorMessage/ErrorMessage.jsx
+++ rest-countries/src/components/ErrorMessage/ErrorMessage.jsx
@@ -3,7 +3,7 @@ import  styles from "./errormessage.module.css"
 export default function ErrorMessage({error}) {
   return (
     
-    <div className={styles.errorMsg}>
-        <i className="fa fa-times-circle"></i>  {error.status} {error.message}</div>
+    <div  className={styles.errorMsg} data-testid="error-message">
+        <i  className="fa fa-times-circle"></i>  {error.status} {error.message}</div>
   )
 }
